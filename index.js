33753db39bf9ff972a1be535ae9d7e2a9f4d20ac
Spencer Hilvitz <spencer.hilvitz@gmail.com>
2017-11-15 07:02:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class BoardMember{
   constructor(name, homestate, training){
     this.name = name;
-    this.homestate = homestate;
+    this.homeState = homeState;
     this.training = training;
   }
 
